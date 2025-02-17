8dbeaa034989eb9284c9ba942d60b6ef3237bbbe
lms96 <luizasalviano@gmail.com>
2025-02-17 10:42:37 -0300

    refactor(web): move ListContainer and InputSearchContainer to homepage

--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -1,3 +1,70 @@
+import { Card, Container, Header, ListContainer, InputSearchContainer } from './styles';
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
+
 export default function Home() {
-  return 'Homepage';
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquise pelo nome" />
+      </InputSearchContainer>
+
+      <Header>
+        <strong>3 contatos</strong>
+        <a href="/">Novo contato</a>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Contato</strong>
+              <small>instagram</small>
+            </div>
+
+            <span>contato@mail.com</span>
+            <span>(84) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Contato</strong>
+              <small>instagram</small>
+            </div>
+
+            <span>contato@mail.com</span>
+            <span>(84) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
+    </Container>
+  );
 }
--- web/src/pages/Home/styles.js
+++ web/src/pages/Home/styles.js
@@ -8,6 +8,7 @@ export const Header = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  margin-top: 32px;
 
   strong {
     font-size: 24px;
@@ -95,5 +96,24 @@ export const Card = styled.div`
       border: none;
       margin-left: 8px;
     }
+  }
+`;
+
+export const InputSearchContainer = styled.div`
+  width: 100%;
+
+  input {
+    width: 100%;
+    background: #fff;
+    border: none;
+    border-radius: 25px;
+    height: 50px;
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+    outline: 0;
+    padding: 0 16px;
+  }
+
+  &::placeholder {
+    color: #bcbcbc;
   }
 `;
