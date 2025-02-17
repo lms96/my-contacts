787d14bd279bfe2c15d93d5ae3d035b01993de8d
lms96 <luizasalviano@gmail.com>
2025-02-17 19:53:31 -0300

    feat(web): create modal component

--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -3,10 +3,13 @@ import { Card, Container, Header, ListContainer, InputSearchContainer } from './
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
+import Modal from '../../components/Modal';
 
 export default function Home() {
   return (
     <Container>
+      <Modal />
+
       <InputSearchContainer>
         <input type="text" placeholder="Pesquise pelo nome" />
       </InputSearchContainer>
