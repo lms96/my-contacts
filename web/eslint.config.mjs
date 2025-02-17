0735c547c00efa6d1abbe405529366d76ab03531
lms96 <luizasalviano@gmail.com>
2025-02-16 22:23:14 -0300

    feat(web): create application header

---
+++ web/eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ['**/*.{js,mjs,cjs,jsx}']},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {rules: {
+    semi: 'error',
+    quotes: [
+      'error',
+      'single',
+      {
+        'avoidEscape': true,
+        'allowTemplateLiterals': true
+      }
+    ],
+    'react/jsx-filename-extension': [
+      1, { 'extensions': ['.js', '.jsx'] }
+    ],
+    'react/react-in-jsx-scope': 'off',
+  }}
+];
