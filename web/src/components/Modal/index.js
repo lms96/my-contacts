781c85830d7fd9dc7d889c2310340d2db1b314ef
lms96 <luizasalviano@gmail.com>
2025-02-17 22:01:50 -0300

    feat(web): render modal using react portal

--- web/src/components/Modal/index.js
+++ web/src/components/Modal/index.js
@@ -1,22 +1,26 @@
 import { Overlay, Container, Footer } from './styles';
 import Button from '../Button';
 import PropTypes from 'prop-types';
+import { createPortal } from 'react-dom';
 
 export default function Modal({ danger }) {
   return (
-    <Overlay>
-      <Container danger={danger}>
-        <h1>Titulo</h1>
-        <p>
-          Corpo
-        </p>
+    createPortal(
+      <Overlay>
+        <Container danger={danger}>
+          <h1>Titulo</h1>
+          <p>
+            Corpo
+          </p>
 
-        <Footer>
-          <button type="button" className="cancel-button">Cancelar</button>
-          <Button type="button" danger={danger}>Deletar</Button>
-        </Footer>
-      </Container>
-    </Overlay>
+          <Footer>
+            <button type="button" className="cancel-button">Cancelar</button>
+            <Button type="button" danger={danger}>Deletar</Button>
+          </Footer>
+        </Container>
+      </Overlay>,
+      document.getElementById('modal-root')
+    )
   );
 }
 
