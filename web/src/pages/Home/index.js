742a4aff2ce91bea014ba47a1506b11da2b5064d
lms96 <luizasalviano@gmail.com>
2025-02-17 23:51:12 -0300

    feat(web): error status in form group and input

--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -3,13 +3,10 @@ import { Card, Container, Header, ListContainer, InputSearchContainer } from './
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
-import Modal from '../../components/Modal';
 
 export default function Home() {
   return (
     <Container>
-      <Modal danger/>
-
       <InputSearchContainer>
         <input type="text" placeholder="Pesquise pelo nome" />
       </InputSearchContainer>
