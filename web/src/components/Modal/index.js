de66c82d74cff7c8381ae333c3bb8661d0692d6f
lms96 <luizasalviano@gmail.com>
2025-02-17 21:19:29 -0300

    feat(web): modal danger status

--- web/src/components/Modal/index.js
+++ web/src/components/Modal/index.js
@@ -1,10 +1,11 @@
 import { Overlay, Container, Footer } from './styles';
 import Button from '../Button';
+import PropTypes from 'prop-types';
 
-export default function Modal() {
+export default function Modal({ danger }) {
   return (
     <Overlay>
-      <Container>
+      <Container danger={danger}>
         <h1>Titulo</h1>
         <p>
           Corpo
@@ -12,9 +13,17 @@ export default function Modal() {
 
         <Footer>
           <button type="button" className="cancel-button">Cancelar</button>
-          <Button type="button">Deletar</Button>
+          <Button type="button" danger={danger}>Deletar</Button>
         </Footer>
       </Container>
     </Overlay>
   );
 }
+
+Modal.propTypes = {
+  danger: PropTypes.bool,
+};
+
+Modal.defaultProps = {
+  danger: false,
+};
