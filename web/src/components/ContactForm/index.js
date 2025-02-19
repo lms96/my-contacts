f79d70cf197f1f57397b66fd1627b4acce707197
lms96 <luizasalviano@gmail.com>
2025-02-19 17:52:34 -0300

    feat(web): handle errors on name and email fields

--- web/src/components/ContactForm/index.js
+++ web/src/components/ContactForm/index.js
@@ -5,25 +5,97 @@ import Input from '../Input';
 import Select from '../Select';
 import Button from '../Button';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { isEmailValid } from '../../utils/isEmailValid.js';
 
 export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+
+    if (!event.target.value) {
+      setErrors((prevState) => [
+        ...prevState,
+        { field: 'name', message: 'Nome é obrigatório.' }
+      ]);
+    } else {
+      setErrors((prevState) => prevState.filter(
+        error => error.field !== 'name'
+      ));
+    }
+  }
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+
+    if (event.target.value && !isEmailValid(event.target.value)) {
+      const errorAlreadyExists = errors.find(error => error.field === 'email');
+      if (errorAlreadyExists) return;
+
+      setErrors((prevState) => [
+        ...prevState,
+        { field: 'email', message: 'E-mail inválido.' }
+      ]);
+    } else {
+      setErrors((prevState) => prevState.filter(
+        error => error.field !== 'email'
+      ));
+    }
+  }
+
+  function getErrorMessageByFieldName(fieldName) {
+    return errors.find(error => error.field === fieldName)?.message;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log({
+      name, email, phone, category
+    });
+  }
+
   return (
-    <Form>
-      <FormGroup>
-        <Input placeholder="Nome" />
+    <Form onSubmit={handleSubmit}>
+      <FormGroup error={getErrorMessageByFieldName('name')}>
+        <Input
+          error={getErrorMessageByFieldName('name')}
+          value={name}
+          placeholder="Nome"
+          onChange={handleNameChange}
+        />
       </FormGroup>
 
-      <FormGroup error="O formato do e-mail é inválido">
-        <Input placeholder="E-mail" error />
+      <FormGroup error={getErrorMessageByFieldName('email')}>
+        <Input
+          error={getErrorMessageByFieldName('email')}
+          value={email}
+          placeholder="E-mail"
+          onChange={handleEmailChange}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Input placeholder="Telefone" />
+        <Input
+          value={phone}
+          placeholder="Telefone"
+          onChange={(event) => setPhone(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Select>
+        <Select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value="">Categoria</option>
           <option value="instagram">Instagram</option>
+          <option value="discord">Discord</option>
         </Select>
       </FormGroup>
 
